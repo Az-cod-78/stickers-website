74e7475483c312586a3384781392a1b2324f56ac
ahmer-naheed-247 <ahmer_naheed@rajaomermustafa.com>
2021-12-16 18:10:08 +0500

    menubar added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,106 +1,6 @@
-@include('layouts.header');
-
+@include('layouts.header')
 <body class="home page-template-default page page-id-180 custom-background wp-custom-logo wp-embed-responsive theme-storefront woocommerce-no-js storefront-full-width-content storefront-align-wide right-sidebar woocommerce-active layout- storefront-pro-active sfp-nav-styleright nav-items-right sfp-shop-layout pootle-page-customizer-active">
-
-
-
-    <div id="page" class="hfeed site">
-        <nav class="secondary-navigation " role="navigation" aria-label="Secondary Navigation">
-            <div class="col-full">
-                <div style='float:left;' class='social-info'></div>
-            </div>
-        </nav>
-        <!-- #site-navigation -->
-
-        <header id="masthead" class="site-header" role="banner" style="">
-
-            <div class="col-full"> <a class="skip-link screen-reader-text" href="#site-navigation">Skip to navigation</a>
-                <a class="skip-link screen-reader-text" href="#content">Skip to content</a>
-                <a href="https://www.sierrastickers.com/" class="site-logo-desktop site-logo-link" rel="home">
-				<img src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%200%200'%3E%3C/svg%3E" alt="Sierra Stickers" data-lazy-src="https://www.sierrastickers.com/wp-content/uploads/2020/10/header-logo-1.svg"/><noscript><img src="https://www.sierrastickers.com/wp-content/uploads/2020/10/header-logo-1.svg" alt="Sierra Stickers"/></noscript>
-			</a>
-                <a href="https://www.sierrastickers.com/" class="site-logo-mobile site-logo-link" rel="home">
-				<img src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%200%200'%3E%3C/svg%3E" alt="Sierra Stickers" data-lazy-src="https://www.sierrastickers.com/wp-content/uploads/2020/10/header-logo-1.svg"/><noscript><img src="https://www.sierrastickers.com/wp-content/uploads/2020/10/header-logo-1.svg" alt="Sierra Stickers"/></noscript>
-
-			</a>
-                <a class="menu-toggle" aria-controls="primary-navigation" aria-expanded="false">
-			<span></span>
-		</a>
-                <nav id="site-navigation" class="main-navigation" aria-label="Primary Navigation">
-                    <div class="sfp-nav-search" style="display: none;">
-
-                        <form role='search' class='search-form' action='https://www.sierrastickers.com/'>
-                            <label class='screen-reader-text' for='s'>Search for:</label>
-                            <input type='search' class='search-field' placeholder='Search&hellip;' value='' name='s' title='Search for:' />
-                            <input type='submit' value='Search' />
-                            <input type='hidden' name='post_type[]' value='post' /><input type='hidden' name='post_type[]' value='page' />
-                        </form>
-                        <a class='sfp-nav-search-close'><i class='fas fa-times'></i></a>
-                    </div>
-                    <!-- .sfp-nav-search -->
-                    <div class="primary-navigation">
-                        <ul id="menu-primary" class="menu">
-                            <li id="menu-item-540" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-540"><a href="#">Create</a></li>
-                            <li id="menu-item-107" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-107"><a href="">Store</a></li>
-                            <li id="menu-item-371" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-371"><a href="">About</a></li>
-                            <li id="menu-item-544" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-544"><a href="">Contact</a></li>
-                            <li id="menu-item-29" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-29"><a href="">Login/Register</a></li>
-                            <li id="menu-item-27" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-27"><a href="">Cart</a></li>
-                            <li class="menu-item wpmenucart wpmenucartli wpmenucart-display-standard menu-item menu-item-type-post_type menu-item-object-page empty-wpmenucart empty" style="">
-
-
-                                <a class="wpmenucart-contents empty-wpmenucart-visible" href="https://www.sierrastickers.com/store/" title="Start shopping">
-		<span class="amount">&#36;0.00</span>
-	</a>
-
-
-
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="handheld-navigation-container">
-                        <div class="handheld-navigation">
-                            <ul id="menu-mobile" class="menu">
-                                <li id="menu-item-720" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-720"><a href="https://www.sierrastickers.com/create/">Create</a></li>
-                                <li id="menu-item-721" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-721"><a href="https://www.sierrastickers.com/store/">Store</a></li>
-                                <li id="menu-item-722" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-722"><a href="https://www.sierrastickers.com/about/">About</a></li>
-                                <li id="menu-item-723" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-723"><a href="https://www.sierrastickers.com/contact/">Contact</a></li>
-                                <li id="menu-item-724" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-724"><a href="https://www.sierrastickers.com/my-account/">Login/Register</a></li>
-                                <li id="menu-item-725" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-725"><a href="https://www.sierrastickers.com/cart/">Cart</a></li>
-                                <li class="menu-item wpmenucart wpmenucartli wpmenucart-display-standard menu-item menu-item-type-post_type menu-item-object-page empty-wpmenucart empty" style="">
-
-
-                                    <a class="wpmenucart-contents empty-wpmenucart-visible" href="https://www.sierrastickers.com/store/" title="Start shopping">
-		<span class="amount">&#36;0.00</span>
-	</a>
-
-
-
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <ul id="site-header-cart" class="site-header-cart menu">
-                        <li class="">
-                            <a class="cart-contents" href="https://www.sierrastickers.com/cart/" title="View your shopping cart">
-								<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#036;</span>0.00</span> <span class="count">0 items</span>
-			</a>
-                        </li>
-                        <li>
-                            <div class="widget woocommerce widget_shopping_cart">
-                                <div class="widget_shopping_cart_content"></div>
-                            </div>
-                        </li>
-                    </ul>
-                </nav>
-                <!-- #site-navigation -->
-            </div>
-            <div class="storefront-primary-navigation">
-                <div class="col-full"></div>
-            </div>
-        </header>
-        <!-- #masthead -->
-
+@include('common.menubar')
 
         <div id="content" class="site-content" tabindex="-1">
             <div class="col-full">
@@ -445,7 +345,72 @@
     <!-- .col-full -->
     </div>
     <!-- #content -->
-@include('layouts.footer');
+
+    <div class="sfb-footer-bar">
+        <div class="col-full">
+            <section id="custom_html-2" class="widget_text widget-odd widget-first widget-1 footer-reviews col-full widget widget_custom_html">
+                <h2 class="widget-title">Nothing But Love From Our Customers</h2>
+                <div class="textwidget custom-html-widget">
+                    <div class="ovals">
+                        <div class="purple"></div>
+                        <div class="green"></div>
+                    </div>
+                    <div class="curated-reviews-container">
+                        <div class="curated-review">
+                            <div class="title-star-container">
+                                <h3 class="curated-review-title">aelkins99</h3>
+                                <div class="curated-star-rating"><span class="stars rating-5"></span></div>
+                            </div>
+                            <p>Good communication, fast shipping, and the stickers turned out great. Thanks again.</p>
+                        </div>
+                        <div class="curated-review">
+                            <div class="title-star-container">
+                                <h3 class="curated-review-title">dcruest</h3>
+                                <div class="curated-star-rating"><span class="stars rating-5"></span></div>
+                            </div>
+                            <p>Will be ordering again in the near future. Great customer service ????.</p>
+                        </div>
+                        <div class="curated-review">
+                            <div class="title-star-container">
+                                <h3 class="curated-review-title">Riley</h3>
+                                <div class="curated-star-rating"><span class="stars rating-5"></span></div>
+                            </div>
+                            <p>The quality is great ????</p>
+                        </div>
+                        <div class="curated-review">
+                            <div class="title-star-container">
+                                <h3 class="curated-review-title">Rachael</h3>
+                                <div class="curated-star-rating"><span class="stars rating-5"></span></div>
+                            </div>
+                            <p>These stickers came out exactly how we wanted them!! Everyone we give them to LOVE them. What I loved most about ordering through Alvalu was how nice they were and they answered all my crazy questions so I didn't feel lost
+                                throughout the upload/design /ordering process. :)</p>
+                        </div>
+                        <div class="curated-review">
+                            <div class="title-star-container">
+                                <h3 class="curated-review-title">Pablo</h3>
+                                <div class="curated-star-rating"><span class="stars rating-5"></span></div>
+                            </div>
+                            <p>Great sticker and print quality</p>
+                        </div>
+                        <div class="curated-review">
+                            <div class="title-star-container">
+                                <h3 class="curated-review-title">Tarsicio</h3>
+                                <div class="curated-star-rating"><span class="stars rating-5"></span></div>
+                            </div>
+                            <p>Great quality stickers! They were a hit.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="custom_html-3" class="widget_text widget-even widget-last widget-2 contact-us widget widget_custom_html">
+                <h2 class="widget-title">Contact Us</h2>
+                <div class="textwidget custom-html-widget">
+                    <div class="wp-block-button aligncenter button-circle"><a href="/contact/" class="wp-block-button__link"><span class="button-text">Contact Us</span></a></div>
+                </div>
+            </section>
+        </div>
+    </div>
+@include('layouts.footer')
 </body>
 
 </html>
